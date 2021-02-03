5b7504c22db5c255d38ae4552259008e0f8ec024
yundeveloper <yuniorbatista1113@gmail.com>
2021-02-03 17:54:02 -0500

    router and routes added to project

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import React, { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Tasks from './components/Tasks';
 import AddTask from './components/AddTask';
+import Footer from './components/Footer';
+import About from './components/About';
 
 function App() {
 	const [
@@ -88,15 +91,27 @@ function App() {
 		setTasks(tasks.map((task) => (task.id === id ? { ...task, reminder: data.reminder } : task)));
 	};
 	return (
-		<div className='container'>
-			<Header title='Task Tracker' toggleDisplay={taskCreatorDisplay} showAdd={showAddTask} />
-			{showAddTask && <AddTask onAdd={addTask} />}
-			{tasks.length > 0 ? (
-				<Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
-			) : (
-				'No Tasks have been recorded!'
-			)}
-		</div>
+		<Router>
+			<div className='container'>
+				<Header title='Task Tracker' toggleDisplay={taskCreatorDisplay} showAdd={showAddTask} />
+				<Route
+					path='/'
+					exact
+					render={(props) => (
+						<React.Fragment>
+							{showAddTask && <AddTask onAdd={addTask} />}
+							{tasks.length > 0 ? (
+								<Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
+							) : (
+								'No Tasks have been recorded!'
+							)}
+						</React.Fragment>
+					)}
+				/>
+				<Route path='/about' component={About} />
+				<Footer />
+			</div>
+		</Router>
 	);
 }
 
