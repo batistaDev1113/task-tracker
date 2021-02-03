e9c70c561f29ba91c3a8ddb0808382ab4d9cd46c
yundeveloper <yuniorbatista1113@gmail.com>
2021-02-03 18:17:07 -0500

    added location.pathname to show/hide button on header conditionally

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,19 +1,23 @@
 import PropTypes from 'prop-types';
 import Button from './Button';
 import { VscTasklist } from 'react-icons/vsc';
+import { useLocation } from 'react-router-dom';
 
 const Header = ({ title, toggleDisplay, showAdd }) => {
+	const location = useLocation();
 	return (
 		<header className='header'>
 			<h1>{title}</h1>
 			<VscTasklist style={{ height: '35px', width: '35px' }} className='task-list' />
-			<Button color={showAdd ? 'red' : 'green'} text={showAdd ? 'Close' : 'Add'} onClick={toggleDisplay} />
+			{location.pathname === '/' && (
+				<Button color={showAdd ? 'red' : 'green'} text={showAdd ? 'Close' : 'Add'} onClick={toggleDisplay} />
+			)}
 		</header>
 	);
 };
 
 Header.propTypes = {
-	title : PropTypes.string.isRequired
+	title: PropTypes.string.isRequired
 };
 
 export default Header;
